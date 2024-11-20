11816af0eb7afda30eee6ee69bb6840d7ece588a
ykmath <matheusmesquitay@gmail.com>
2024-11-19 22:57:35 -0400

    Agora é possivel sair do app utilizando o botão de voltar nativo do celular

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import useDb, { DBContext } from './components/db/useDb';
 import { NotificationConfig, NotificationContext } from './components/Notification';
+
+import { App as application } from "@capacitor/app"
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -17,6 +19,19 @@ export const AppContext = createContext<AppContextProps | null>(null);
 const Main = () => {
   const dbVariables = useDb();
   const [notification, setNotification] = useState<NotificationConfig | null>(null);
+
+  useEffect(() => {
+    const listener = application.addListener("backButton", (e) => {
+      const loc = window.location.pathname;
+      if (loc === "/" || loc === "/home") {
+        application.exitApp();
+      }
+    })
+
+    return () => {
+      listener.then((ev) => ev.remove());
+    }
+  }, [])
   
   return (
     <React.StrictMode>
