9cff66c4b17ad4d77526c238e151b781d4893e42
ykmath <matheusmesquitay@gmail.com>
2024-11-20 15:00:34 -0400

    Remoção de um código antigo inutilizavel

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -19,11 +19,6 @@ const Header = ({type="small-header", text, goBack}: HeaderProps) => {
         <IonHeader id="header" className={type}>
             <IonToolbar id="toolbar">
                 {goBack && (
-                    // <IonButtons id="header-icon-container" slot="start">
-                    //     <IonButton id="header-icon" onClick={returnPage}>
-                    //         <IonIcon id="icon" icon={chevronBack}></IonIcon>
-                    //     </IonButton>
-                    // </IonButtons>
                     <button id="header-icon-container" onClick={returnPage}>
                         <FaAngleLeft id="header-icon" />
                     </button>
